f210b115adf7a5f15903dc5858f9ba200da94319
harry@ardimedia.com <harry@ardimedia.com>
2019-10-27 10:22:26 +0100

    feature(db): database scheams and database migration

---
+++ IdentityMigrationDb/migration-step-90-data.sql
@@ -0,0 +1,7 @@
+﻿-- MIGRATE DATA
+UPDATE dbo.AspNetRoles SET NormalizedName = UPPER(Name);
+GO
+UPDATE dbo.AspNetUsers SET NormalizedEmail = UPPER(Email);
+GO
+UPDATE dbo.AspNetUsers SET NormalizedUserName = UPPER(UserName);
+GO
